dba8d5edbecc72337315d51a6e172d468f0cd765
mathely27 <mathely27@gmail.com>
2026-08-23 20:45:21 +0530

    Minor changes

--- src/Components/ActiveModules.jsx
+++ src/Components/ActiveModules.jsx
@@ -21,7 +21,7 @@ function ActiveModules(){
     </div>
     <div className="module-card">
         <Keyboard />
-        <h3>MEMORY TEST</h3>
+        <h3>COLOUR CLASH</h3>
         <p>Challenge your memory and focus.</p>
         <button>LAUNCH</button>
     </div>
@@ -29,28 +29,28 @@ function ActiveModules(){
 
     <div className="module-card">
         <Timer />
-        <h3>REACTION TIME</h3>
+        <h3>NUMBER RUSH</h3>
         <p>Test how quickly you can react.</p>
         <button>LAUNCH</button>
     </div>
 
     <div className="module-card">
         <Keyboard />
-        <h3>WORD SPRINT</h3>
+        <h3>FLASH RECALL</h3>
         <p>Challenge your speed with words.</p>
         <button>LAUNCH</button>
     </div>
 
     <div className="module-card">
         <Mouse />
-        <h3>AIM TRAINER</h3>
+        <h3>DONT'T CLICK</h3>
         <p>Improve your clicking precision.</p>
         <button>LAUNCH</button>
     </div>
 
     <div className="module-card">
         <Timer />
-        <h3>REFLEX RUSH</h3>
+        <h3>QUICK DRAW</h3>
         <p>Push your reflexes to the limit.</p>
         <button>LAUNCH</button>
     </div>
@@ -64,7 +64,7 @@ function ActiveModules(){
 
     <div className="module-card">
         <Mouse />
-        <h3>PRECISION CLICK</h3>
+        <h3>REFLEX SWITCH</h3>
         <p>Test your accuracy and control.</p>
         <button>LAUNCH</button>
     </div>
